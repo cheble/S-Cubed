14c6bada94118050b61f6eb9649be110b82a04a7
cheble <chase.heble@gmail.com>
2015-03-18 16:12:35 -0500

    fix lost commits

---
+++ _includes/details.js
@@ -0,0 +1,67 @@
+<script>
+	var Details ;
+    $(document).ready(function() {
+    	//DOM sensitive logic
+    	Details = new Details(getParameter("d"));
+    	Details.setUp();
+    });
+
+
+    var Details = function(d){
+
+    	var context = this;
+
+    	var name = d;
+
+    	var arrTags[];
+
+    	var sImage;
+
+    	var sDtails;
+
+    	var sWebsite;
+
+    	var sDownload;
+
+    	var arrLinks[]; //array ( of link )
+
+    	var nFavorites;
+
+    	var arrComments[]; //arrary ( of comment )
+
+    	//CONSTANTS//
+	    var name = "name";
+	    var tags = "tags";
+	    var image = "image";
+	    var details = "details";
+	    var website = "website";
+	    var download = "download";
+	    var Links = "Links";
+	    var favorites = "favorites";
+	    var comments = "comments";
+	    //--------//
+
+		this.setUp = function(){
+			$.getJSON( "Details/"+name+".json", function( data ) {
+				var stopping = data;
+			}
+		}
+
+		var link = function(description,url){
+
+	    	this.description = description;
+	    	this.url = url;
+
+	    }
+
+	    var comment = function(username,comment){
+
+	    	this.username = username;
+	    	this.comment = comment;
+	    }
+
+
+    }
+
+    
+</script>
