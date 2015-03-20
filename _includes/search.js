e41dbceb51beebcd3a18684bb29b290c0778fba1
cheble <chase.heble@gmail.com>
2015-03-19 20:23:33 -0500

    listing page progress[B

--- _includes/search.js
+++ _includes/search.js
@@ -48,9 +48,9 @@ var Search = function(q){
   for (product in products) {
     console.log(product);
     console.log(query);
-    if (query.toUpperCase() === product.id.toUpperCase() ||
-          query.toUpperCase() === product.name.toUpperCase()) {
-      matchedIds.push(product.id);
+    // if (query.toUpperCase() === product.id.toUpperCase() ||
+    //       query.toUpperCase() === product.name.toUpperCase()) {
+    //   matchedIds.push(product.id);
     }
   }
 
