0f2d156124556c275fdfbf92ae85def7ac4d103a
cheble <chase.heble@gmail.com>
2015-04-30 13:17:21 -0500

    tuned sorting compare functions (fix)

--- _includes/search.js
+++ _includes/search.js
@@ -105,11 +105,39 @@ var Search = function(q){
   }
 
   this.compareProductByFavorites = function(a, b) {
-    return a.favorites - b.favorites;
+    if (a.favorites < b.favorites) {
+      return -1;
+    } else if (a.favorites == b.favorites) {
+      // compare by name if equal
+      if (a.name < b.name) {
+        return -1;
+      } else if (a.name == b.name) {
+        return 0;
+      } else {
+        return 1;
+      }
+
+    } else {
+      return 1;
+    }
   }
 
   this.compareProductByComments = function(a, b) {
-    return a.comments - b.comments;
+    if (a.comments < b.comments) {
+      return -1;
+    } else if (a.comments == b.comments) {
+      // compare by name if equal
+      if (a.name < b.name) {
+        return -1;
+      } else if (a.name == b.name) {
+        return 0;
+      } else {
+        return 1;
+      }
+
+    } else {
+      return 1;
+    }
   }
 
 }
